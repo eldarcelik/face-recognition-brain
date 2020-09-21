d378cc0ad7d5d1123f6230adb5cb1a12cf6c2b77
Eldar Celik <eldar.celik21@gmail.com>
2020-09-21 18:07:51 +0200

    Updates

---
+++ precache-manifest.ab413117c113c95087df8c8d27d60a17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2611f4dbe1f16469089ef2a643f969fb",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "ed324552fd20676e3368",
+    "url": "/face-recognition-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ebe96594154751740f7a",
+    "url": "/face-recognition-brain/static/css/main.61cfbb8f.chunk.css"
+  },
+  {
+    "revision": "ed324552fd20676e3368",
+    "url": "/face-recognition-brain/static/js/2.82a92f06.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recognition-brain/static/js/2.82a92f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebe96594154751740f7a",
+    "url": "/face-recognition-brain/static/js/main.223ebf24.chunk.js"
+  },
+  {
+    "revision": "56148025b8b7094fd937",
+    "url": "/face-recognition-brain/static/js/runtime-main.fa010191.js"
+  }
+]);
