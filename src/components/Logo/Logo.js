aea05b206715567ae06532c0cb15baf44882f033
Eldar Celik <eldar.celik21@gmail.com>
2020-12-24 19:18:25 +0100

    Added photo to README.md

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Tilt from 'react-tilt';
-import brain from './brain.png';
+import brain from '../../images/brain.png';
 import './Logo.css';
 
 const Logo = () => {
